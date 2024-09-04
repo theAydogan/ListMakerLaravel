fffbdfd74521c31ab812029b4df795e176c5afd4
theAydogan <osmanahmet13@gmail.com>
2024-09-04 15:07:39 -0400

    Login UI updated with more simple layout and some illustrations

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,12 +3,7 @@
 
 <head>
     <title>Login Page</title>
-</head>
-<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
-<body>
-
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <!-- <nav class="border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-right justify-between mx-auto p-4">
             <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
@@ -30,33 +25,45 @@
                 </ul>
             </div>
         </div>
-    </nav>
+    </nav> -->
+</head>
+<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
+<body>
     <div class="bg-white flex justify-center items-center h-screen">
-        <div class="w-1/2 h-screen hidden lg:block">
-            <img src="{{ asset('logo.png') }}" alt="Placeholder Image" class="object-center w-full mt-10 ml-10">
+        <div class="flex items-center justify-center w-1/2 h-1/2 bg-white">
+            <img src="{{ asset('userLoginPic.png') }}" alt="Placeholder Image" class="h-full">
         </div>
-        <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-            <h1 class="text-2xl font-semibold mb-4">Login</h1>
-            <form action="/attempt_login" method="POST">
-                @csrf 
-                <div class="mb-4">
-                    <label for="email" class="block text-gray-600">Email</label>
-                    <input type="text" id="username" name="email" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off">
-                </div>
-                <div class="mb-4">
-                    <label for="password" class="block text-gray-600">Password</label>
-                    <input type="password" id="password" name="password" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off">
-                </div>
-                <div class="mb-4 flex items-center">
-                    <input type="checkbox" id="remember" name="remember" class="text-blue-500">
-                    <label for="remember" class="text-gray-600 ml-2">Remember Me</label>
-                </div>
-                <div class="mb-6 text-blue-500">
-                    <a href="#" class="hover:underline">Forgot Password?</a>
-                </div>
-                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">Login</button>
-            </form>
+        <div class="bg-yellow-400 h-full w-1/2 flex flex-col items-center justify-center">
+
+            <div>
+                <h1 class="text-8xl m-4 font-extrabold leading-none tracking-tight">LISTMKR</h1>
+            </div>
+            <div>
+                <h1 class="text-2xl font-semibold mb-4">Log in to your Account</h1>
+                <h3 class="text-1xl font-semibold text-gray-700 text-center">Make your list today!</h3>
+            </div>
+            <div class="w-1/2">
+                <form action="/attempt_login" method="POST">
+                    @csrf
+                    <div class="mb-4">
+                        <label for="email" class="block m-2 text-gray-600">Email</label>
+                        <input type="text" id="username" name="email" class="w-full text-base border border-gray-300 rounded-full py-2 px-5 focus:outline-none focus:border-blue-500" autocomplete="off">
+                    </div>
+                    <div class="mb-4">
+                        <label for="password" class="block m-2 text-gray-600">Password</label>
+                        <input type="password" id="password" name="password" class="w-full border border-gray-300 rounded-full py-2 px-5 focus:outline-none focus:border-blue-500" autocomplete="off">
+                    </div>
+                    <div class="mb-4 flex items-center">
+                        <input type="checkbox" id="remember" name="remember" class="text-blue-500">
+                        <label for="remember" class="text-gray-600 ml-2">Remember Me</label>
+                    </div>
+                    <div class="mb-6 text-blue-500">
+                        <a href="#" class="hover:underline">Forgot Password?</a>
+                    </div>
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">Login</button>
+                </form>
+            </div>
 
             @if ($errors->any())
             <div>
@@ -73,7 +80,7 @@
         </div>
     </div>
 
-    <div class="py-16 bg-yellow-400">
+    <!-- <div class="py-16 bg-yellow-400">
         <div class="container m-auto px-6 text-gray-500 md:px-12 xl:px-0">
             <div class="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-3">
                 <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
@@ -102,10 +109,10 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 
-    <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 w-full">
-        <div class="max-w-2xl mx-auto" >
+    <footer class="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 w-full">
+        <div class="max-w-2xl mx-auto">
             <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" class="hover:underline" target="_blank">Ahmet Tech Enterprises™</a>. All Rights Reserved.</span>
             <ul class="flex flex-wrap items-center mt-3 sm:mt-0">
                 <li>
